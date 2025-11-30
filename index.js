8e6cb8d34cb78c79e076839bb5d8cc43e60e74f6
Arseniy.shrk <arseniy.shrk@gmail.com>
2025-11-30 22:38:33 +0700

    v0.3 (added head to table (now work normal only at 1920 * 1080 ))

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const slider = document.querySelector('.slider')
 const slider_text = document.querySelector('.slider_text')
 const sel_lang = document.querySelector('.select_language')
 const _h1 = document.querySelector('._h1')
+const cell_size = body.offsetHeight / 30
+const run_button = document.querySelector('.run_button')
+const delete_button = document.querySelector('.delete_button')
+const speed_button = document.querySelector('.change_speed')
+const fill_button = document.querySelector('.fill_button')
 slider.value = 0.5
 slider_text.innerHTML = slider.value + 's'; 
-
-const cell_size = body.offsetHeight / 30
 sel_lang.style.fontSize = sel_lang.offsetHeight * 0.33 + 'px'
 _h1.style.height = body.offsetHeight * 0.1 + 'px'
 _h1.style.fontSize = _h1.offsetHeight * 0.33 + 'px'
@@ -20,17 +23,23 @@ board.style.height = cell_size * cl + 'px';
 table.style.width = cell_size * st + 20 + 'px'
 table.style.height = (cell_size * cl) * 1.1 + 'px' 
 table_head.style.width = cell_size * st + 'px'
+const len_to_run = delete_button.offsetWidth + speed_button.offsetWidth + fill_button.offsetWidth + run_button.offsetWidth * 2
+run_button.style.left = (table_head.offsetWidth - len_to_run) + 'px'
 
 console.log('[' + Date().toLocaleString() + ']')
 console.log('window: (' + window.screen.width + ', ' + window.screen.height + ')')
 console.log('skreen: (' + screen.width + ', ' + screen.height + ')')
 console.log('body: (' + body.offsetWidth + ', ' + body.offsetHeight + ')')
-console.log('board     <= .cell_container')
-console.log('table     <= .table')
-console.log('table_head<= .table_head')
-console.log('sel_lang  <= .select_language')
-console.log('_h1       <= ._h1')
-console.log('cell_size <= ' + body.offsetHeight / 40)
+console.log('board         <= .cell_container')
+console.log('table         <= .table')
+console.log('table_head    <= .table_head')
+console.log('delete_button <= .delete_button')
+console.log('speed_button  <= .change_speed')
+console.log('fill_button   <= .fill_button')
+console.log('run_button    <= .run_button')
+console.log('sel_lang      <= .select_language')
+console.log('_h1           <= ._h1')
+console.log('cell_size     <= ' + body.offsetHeight / 40)
 console.log(' ')
 console.log('[' + Date().toLocaleString() + ']')
 console.log('Apply sel_lang.style.fontSize = ' + sel_lang.offsetHeight * 0.33 + 'px')
@@ -39,6 +48,7 @@ console.log('Apply _h1.style.fontSize = ' + _h1.offsetHeight * 0.33 + 'px')
 console.log('Apply board.style.width = ' + cell_size * st + 'px')
 console.log('Apply board.style.height = ' + cell_size * cl + 'px')
 console.log('Apply table.style.width = ' + cell_size * st + 20 + 'px')
+console.log(`Apply run_button.style.left = ${table_head.offsetWidth - len_to_run} px`)
 console.log(' ')
 console.log('[' + Date().toLocaleString() + ']')
 
@@ -54,5 +64,6 @@ for (let i = 0; i < cl; i++) {
 }
 
 slider.oninput = function() {
-    slider_text.innerHTML = this.value + 's';
+        slider_text.innerHTML = this.value + 's';
+        console.log('slider.value = ' + this.value  )
 }
